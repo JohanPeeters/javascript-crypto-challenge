cf0df35d14e2b8bb16abbe7c9822830e1d89d05e
EZ ZYN Abdelali <43846739+geekzain@users.noreply.github.com>
2019-10-07 16:39:27 +0200

    Decrypting a ciphertext (test working)

--- src/Decryptor.js
+++ src/Decryptor.js
@@ -1,1 +1,21 @@
+// Abdelali Ez Zyn
+// Sources:
+// https://github.com/wilhelmmatilainen/natrium
 
+// Load sodium-wrappers module
+const sodium = require('libsodium-wrappers');
+let key = null;
+
+// Generates key
+module.exports.setKey = async function setKey(_key) {
+	key = _key;
+};
+
+// Decrypts the encrypted message when you have the key
+module.exports.decrypt = async function decrypt(ciphertext, nonce) {
+	if (key == null) {
+		throw 'no key';
+	} else {
+		return sodium.crypto_secretbox_open_easy(ciphertext, nonce, key);
+	}
+};
